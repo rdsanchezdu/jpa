f3132e08416418aa8ea11f0bd150fa5a2e2cf877
Roberto Sanchez <135069409+rdsanchezdu@users.noreply.github.com>
2024-01-15 05:28:57 -0500

    feat: client test is working correctly
    Has been added connection holder configuration and corrected bugs

--- src/test/java/co/com/rob/jpa/ClientDbUnitArquillianTest.java
+++ src/test/java/co/com/rob/jpa/ClientDbUnitArquillianTest.java
@@ -1,76 +1,140 @@
-//package co.com.rob.jpa;
-//
-//import co.com.rob.jpa.entities.Expense;
-//import com.github.database.rider.core.api.connection.ConnectionHolder;
-//import com.github.database.rider.core.api.dataset.DataSet;
-//import com.github.database.rider.junit5.DBUnitExtension;
-//import org.jboss.arquillian.container.test.api.Deployment;
-//import org.jboss.arquillian.junit5.ArquillianExtension;
-//import org.jboss.shrinkwrap.api.ShrinkWrap;
-//import org.jboss.shrinkwrap.api.asset.FileAsset;
-//import org.jboss.shrinkwrap.api.spec.WebArchive;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//
-//import java.io.File;
-//import java.sql.*;
-//import java.util.Enumeration;
-//
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//@ExtendWith(ArquillianExtension.class)
-//@ExtendWith(DBUnitExtension.class)
-//public class ClientDbUnitArquillianTest {
-//
-//    // java:/jdbc/personalBudgetDS
-//    ConnectionHolder buildConnectionHolder() {
-//        System.out.println("***************** getting connection");
-//        Connection connection = null;
-//        try {
-//            Enumeration<Driver> drivers = DriverManager.getDrivers();
-//            while (drivers.hasMoreElements()) {
-//
-//                Driver driver = drivers.nextElement();
-//                System.out.println("Driver: " + driver.getClass().getName());
-//            }
-//            connection = DriverManager.getConnection(
-//                    "jdbc:mysql://localhost:3307/personal_budget?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true"
-//                    , "budget"
-//                    , "budget");
-//        } catch (SQLException e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println(connection);
-//
-//
-//        return () -> DriverManager.getConnection("jdbc:mysql://host.docker.internal:3307/personal_budget" +
-//                        "?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true"
-//                , "budget"
-//                , "budget");
-//    }
-//
-//    @Deployment(testable = false)
-//    public static WebArchive createDeployment() {
-//        return ShrinkWrap.create(WebArchive.class, "jpa2-test.war")
-//                .addClass(Expense.class)
-//                .addAsResource(new FileAsset(new File("src/main/resources/META-INF/persistence.xml")),
-//                        "/META-INF/persistence.xml");
-//    }
-//
-//    @Test
-//    @DataSet(value = "/datasets/expenses.yml")
-//    void testFindExpense() throws SQLException {
-//        System.out.println("**************Entra a hacer el test");
+package co.com.rob.jpa;
+
+import co.com.rob.jpa.entities.Expense;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import com.github.database.rider.core.api.connection.ConnectionHolder;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.junit5.DBUnitExtension;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit5.ArquillianExtension;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.FileAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.io.File;
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@ExtendWith(DBUnitExtension.class)
+public class ClientDbUnitArquillianTest {
+
+    ConnectionHolder buildConnectionHolder() throws ClassNotFoundException {
+        //It is necessary to have installed mysql dependency in pom.xml file
+        //if not this will not work.
+        return () -> DriverManager.getConnection(
+                  "jdbc:mysql://localhost:3307/personal_budget?autoReconnect=true&useSSL=false&allowPublicKeyRetrieval=true"
+                , "budget"
+                , "budget");
+    }
+
+
+    @Deployment(testable = false)
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class, "jpa2-test.war")
+                .addClass(Expense.class)
+                .addAsResource(
+                        new FileAsset(
+                                new File(
+                                        "src/main/resources/META-INF/persistence.xml")),
+                                        "/META-INF/persistence.xml");
+    }
+
+    @Test
+    @DataSet(value = "/datasets/expenses.yml")
+        void testFindExpense() throws SQLException {
+        try (Connection connection = buildConnectionHolder().getConnection();
+             PreparedStatement ps = connection.prepareStatement("SELECT * from expenses where id = ?");) {
+            ps.setLong(1, 1L);
+            ResultSet resultSet = ps.executeQuery();
+            System.out.println(resultSet.next());
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(3));
+            System.out.println(resultSet.getString(5));
+            assertEquals("1",resultSet.getString(1) );
+            //assertTrue(resultSet.next());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    //este test usa un connection holder uqe está creado en una
+    //clase interna y crea una base de datos con 4 valores.
+    @Test
+    @DataSet(value = "/datasets/expenses_con_varios_registros.yml")
+    void testFindExpense2() throws SQLException {
+        System.out.println("**************Entra a hacer el test");
 //        System.out.println("buildConnectionHolder: "+ buildConnectionHolder());
-//        try (Connection connection = buildConnectionHolder().getConnection();
-//             PreparedStatement ps = connection.prepareStatement("SELECT id,amount,comments,concept from expenses where id = ?");) {
-//            ps.setLong(1, 1L);
-//            ResultSet resultSet = ps.executeQuery();
-//
-//            assertTrue(resultSet.next());
-//        }
-//    }
-//}
-////    Bound non-transactional data source: java:/jdbc/personalBudgetDS
-////WFLYJPA0002: Read persistence.xml for persistenceUnit-personalBudget
-////        Http management interface listening on http://127.0.0.1:10090/management
+
+        try {
+            ConnectionHolderInternClass myConnectionInternClass = new ConnectionHolderInternClass();
+            System.out.println("entra a llamar getConnection");
+            Connection connection2 =  myConnectionInternClass.getConnection();
+             PreparedStatement ps =
+                        connection2.prepareStatement(
+                                "SELECT id,amount,comments,concept " +
+                                        "from expenses " +
+                                        "where id = ?"
+                        );
+            ps.setLong(1, 1L);
+            ResultSet resultSet = ps.executeQuery();
+            System.out.println("*****antes del assert");
+            System.out.println(resultSet.next());
+            System.out.println(resultSet.getString(1));
+            System.out.println(resultSet.getString(2));
+            System.out.println(resultSet.getString(3));
+            assertEquals("120.43", resultSet.getString(2));
+            //assertTrue(resultSet.next());
+
+        } catch (Exception e){
+            System.out.println("exception");
+            System.out.println(e);
+
+        }
+    }
+
+}//fin de la clase publica.
+
+class ConnectionHolderInternClass {
+
+    private  ThreadLocal<Connection> connectionHolder = new ThreadLocal<>();
+
+    public Connection getConnection() throws SQLException, ClassNotFoundException {
+        System.out.println("********entra buscando la conexión");
+        Connection connection = connectionHolder.get();
+        if (connection == null || connection.isClosed()) {
+            System.out.println("la conexion es nula");
+            //Crear nueva conexión si no hay una disponible o está cerrada
+            connection = createNewConnection();
+            connectionHolder.set(connection);
+        }
+        System.out.println("connection: "+connection);
+        return connection;
+    }
+
+    public  void releaseConnection() {
+        Connection connection = connectionHolder.get();
+        if (connection != null) {
+            // No cerrar la conexión aquí; solo marcarla como disponible para su reutilización
+            connectionHolder.remove();
+        }
+    }
+
+    private  Connection createNewConnection() throws SQLException, ClassNotFoundException {
+        // Implementar la lógica para crear una nueva conexión aquí
+//        Class.forName("com.mysql.cj.jdbc.Driver");
+        System.out.println("*****entra al connection holder a crear una conexión");
+        //IMPORTANT:
+        return DriverManager.getConnection( "jdbc:mysql://localhost:3307/personal_budget?autoReconnect=true&amp",
+                                            "budget",
+                                            "budget");
+    }
+
+}
+
--- src/test/java/co/com/rob/jpa/JpaArquillianTest.java
+++ src/test/java/co/com/rob/jpa/JpaArquillianTest.java
@@ -1,9 +1,8 @@
 package co.com.rob.jpa;
 
 import co.com.rob.jpa.entities.Expense;
-//import co.com.rob.jpa.extensions.ArquillianDBUnitExtension;
-//import com.github.database.rider.core.api.dataset.DataSet;
-//import com.github.database.rider.junit5.DBUnitExtension;
+import co.com.rob.jpa.extensions.ArquillianDBUnitExtension;
+import com.github.database.rider.core.api.dataset.DataSet;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -20,57 +19,58 @@ import jakarta.annotation.Resource;
 import javax.sql.DataSource; //TODO: intentar cambiarlo a jakarta y ver que pasa
 import java.io.File;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
-//import com.github.database.rider.core.api.connection.ConnectionHolder;
+import com.github.database.rider.core.api.connection.ConnectionHolder;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 @ExtendWith(ArquillianExtension.class)
-//@ExtendWith(ArquillianDBUnitExtension.class)
+@ExtendWith(ArquillianDBUnitExtension.class)
 class JpaArquillianTest {
 
-//    @Resource(lookup = "java:/jdbc/personalBudgetDS")
-//    private DataSource dataSource;
+    @Resource(lookup = "java:/jdbc/personalBudgetDS")
+    private DataSource dataSource;
 
     @PersistenceContext
     private EntityManager em;
 
     private Connection connection;
 
-//    ConnectionHolder buildConnectionHolder() {
-//        return () -> {
-//            if (connection == null || connection.isClosed()) {
-//                connection = dataSource.getConnection();
-//            }
-//            return connection;
-//        };
-//    }
+    //este connecion holder es para databaserider
+    //la cual llenará la base de datos con lo que pongamos en expenses.yml
+    ConnectionHolder buildConnectionHolder() {
+        return () -> {
+            if (connection == null || connection.isClosed()) {
+                connection = dataSource.getConnection();
+            }
+            return connection;
+        };
+    }
 
     @Deployment
     public static WebArchive createDeployment() {
-//        File[] dbRider = Maven.resolver()
-//                .loadPomFromFile("pom.xml")
-//                .resolve("com.github.database-rider:rider-junit5")
-//                .withTransitivity()
-//                .asFile();
+        File[] dbRider = Maven.resolver()
+                .loadPomFromFile("pom.xml")
+                .resolve("com.github.database-rider:rider-junit5")
+                .withTransitivity()
+                .asFile();
 
         return ShrinkWrap.create(WebArchive.class, "jpa-test.war")
                 .addClass(Expense.class)
                 .addAsWebInfResource(new File("src/test/resources/dbunit.yml"), "classes/dbunit.yml")
                 .addAsResource(new FileAsset(new File("src/main/resources/META-INF/persistence.xml")),
                         "/META-INF/persistence.xml")
-                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml");
-//                .addClass(ArquillianDBUnitExtension.class)
-//                .addAsWebInfResource(new File("src/test/resources/datasets/expenses.yml"), "classes/datasets/expenses.yml")
-//                .addAsLibraries(dbRider);
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
+                .addClass(ArquillianDBUnitExtension.class)
+                .addAsWebInfResource(new File("src/test/resources/datasets/expenses.yml"), "classes/datasets/expenses.yml")
+                .addAsWebInfResource(new File("src/test/resources/datasets/expenses_con_varios_registros.yml"), "classes/datasets/expenses_con_varios_registros.yml")
+                .addAsLibraries(dbRider);
     }
 
     @Test
-    //@DataSet(value = "/datasets/expenses.yml")
+    @DataSet(value = "/datasets/expenses_con_varios_registros.yml")
     void testFindExpenseShort() throws SQLException {
         Expense expense = em.find(Expense.class, 1L);
         System.out.println("expense: " + expense.toString());
